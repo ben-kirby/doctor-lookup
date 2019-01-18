2722013083ec14ee62cc2d0fe961ab0cbcb4d019
Ben Kirby <benkrby@gmail.com>
2019-01-18 09:16:00 -0800

    Now has the ability to search PDX doctors by name

--- src/lookup.js
+++ src/lookup.js
@@ -1,8 +1,15 @@
 class Lookup {
   static searchBySymptom(symptom){
     return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&query=${symptom}&location=or-portland`).then(function(response){
-      return response.json()
+      return response.json();
+    })
+  }
+
+  static searchByName(name){
+    return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&name=${name}&location=or-portland`).then(function(response){
+      return response.json();
     })
   }
 }
+
 export { Lookup }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { Lookup } from './lookup';
 import $ from 'jquery';
+import 'bootstrap';
 
 $(document).ready(function(){
-  let doctors = Lookup.searchBySymptom("cough");
-  console.log(doctors);
+  // let doctorsBySymptom = Lookup.searchBySymptom("cough");
+  // console.log("symptoms", doctorsBySymptom);
+
+  let doctorsByName = Lookup.searchByName("smith");
+  console.log("name", doctorsByName);
 });
