060ce89cc3760732b381119cd57a159bef34afd1
Ben Kirby <benkrby@gmail.com>
2019-01-18 11:05:56 -0800

    Now has a json object to work with as opposed to a promise object.

--- src/lookup.js
+++ src/lookup.js
@@ -1,8 +1,7 @@
 class Lookup {
+
   static searchBySymptom(symptom){
-    return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&query=${symptom}&location=or-portland`).then(function(response){
-      return response.json();
-    })
+    return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&query=${symptom}&location=or-portland`)
   }
 
   static searchByName(name){
--- src/main.js
+++ src/main.js
@@ -1,11 +1,32 @@
 import { Lookup } from './lookup';
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 $(document).ready(function(){
-  // let doctorsBySymptom = Lookup.searchBySymptom("cough");
-  // console.log("symptoms", doctorsBySymptom);
+  let results = null;
+  document.getElementById('symptom-search').addEventListener("click", function(){
+    let searchTerm = document.getElementById('symptom-query').value;
+    console.log(searchTerm);
 
-  let doctorsByName = Lookup.searchByName("smith");
-  console.log("name", doctorsByName);
+    Lookup.searchBySymptom(searchTerm).then(function(symptomResponse){
+      return symptomResponse.json();
+    }).then(function(docBySymptom) {
+      results = docBySymptom;
+    }).then(function() {
+      console.log(results);
+
+    });
+  });
+
+  // document.getElementById("name-search").addEventListener("click", function(){
+  //   let searchTerm = document.getElementById('name-query');
+  //   results = Lookup.searchByName(searchTerm);
+  // });
+  //
+  // let displayResults = function(results){
+  //
+  // }
+
 });
