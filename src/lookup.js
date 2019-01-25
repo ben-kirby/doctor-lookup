0c4ec154c2c57b37c5de9a86f96807b973577216
Ben Kirby <benkrby@gmail.com>
2019-01-24 18:56:16 -0800

    Revert changes in Lookup and Main. Rename API var for grading.

--- src/lookup.js
+++ src/lookup.js
@@ -1,40 +1,11 @@
-import $ from 'jquery';
-
 class Lookup {
 
   static searchBySymptom(symptom){
-    // $.ajax({
-    //   url: `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&query=${symptom}&location=or-portland`,
-    //   type: 'GET',
-    //   data: {
-    //     format: 'json'
-    //   },
-    //   success: function(response) {
-    //     return response;
-    //   },
-    //   error: function(error) {
-    //     return new Error(error.statusText)
-    //   }
-    // });
-    //
-    // .then(function(response){
-    //   if (!response.ok) {
-    //     throw Error(response.statusText)
-    //   }
-    //   return response;
-    // }).then(function(response){
-    //   return response;
-    // }).catch(function(error){
-    //   return error;
-    // })
-
-
-
-    return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&query=${symptom}&location=or-portland`)
+    return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${exports.apiKey}&query=${symptom}&location=or-portland`)
   }
 
   static searchByName(name){
-    return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&name=${name}&location=or-portland`)
+    return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?user_key=${exports.apiKey}&name=${name}&location=or-portland`)
   }
 }
 
